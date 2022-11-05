ab9113dc7e91e97dda854d7ffc7d8a88a88555c1
HimbeerserverDE <himbeerserverde@gmail.com>
2022-11-05 20:54:33 +0100

    completely exclude None options

--- src/util/value.rs
+++ src/util/value.rs
@@ -309,7 +309,7 @@ where
     }
 
     fn serialize_none(self) -> Result<Self::Ok> {
-        self.serialize_unit()
+        Ok(())
     }
 
     fn serialize_some<T>(self, v: &T) -> Result<Self::Ok>
